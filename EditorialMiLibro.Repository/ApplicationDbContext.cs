6a7a0a1e8a89f6be7461b7f678486ba9d98da27e
Erickson L <linobarrantes@gmail.com>
2023-05-04 17:54:22 -0500

    Revert "Revert "Creación de clase DbContext, inyección de DbContext creado en Program, creación de la""
    This reverts commit 6d52a437aefaa6079be9552e79985fae1d749965.

---
+++ EditorialMiLibro.Repository/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EditorialMiLibro.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace EditorialMiLibro.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Libro> MisLibros { get; set; }   
+    }
+}
