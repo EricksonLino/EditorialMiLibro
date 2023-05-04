577ffef867f1d0e1c048dbdcd64d0130d792ed38
Erickson L <linobarrantes@gmail.com>
2023-05-04 13:38:46 -0500

    Creación de clase DbContext, inyección de DbContext creado en Program, creación de la

--- EditorialMiLibro.API/Program.cs
+++ EditorialMiLibro.API/Program.cs
@@ -1,6 +1,11 @@
+using EditorialMiLibro.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(conf => 
+                            conf.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ EditorialMiLibro.Repository/Migrations/20230504181548_InitialMigration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EditorialMiLibro.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MisLibros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Autor = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Descripción = table.Column<string>(type: "nvarchar(350)", maxLength: 350, nullable: false),
+                    Género = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FechadePublicación = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EjemplaresProducidas = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MisLibros", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MisLibros");
+        }
+    }
+}
